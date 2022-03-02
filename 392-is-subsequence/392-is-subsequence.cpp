5818e11c8d79b60468db520213ff1f57dac9cf86
Chandan Giri <jpgiri017@gmail.com>
2022-03-02 20:54:55 +0530

    Time: 2 ms (52.38%), Space: 6.4 MB (72.95%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int j=0;
+        for(int i=0;i<t.length();i++)
+        {
+            if( j < s.length() && t[i]==s[j])
+            {
+                j++;
+            }
+        }
+        
+        if(j==s.length())
+            return true;
+        return false;
+        
+    }
+};
