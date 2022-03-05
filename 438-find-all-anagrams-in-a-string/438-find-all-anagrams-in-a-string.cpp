660bf476e015393b8f2029932561ab72ad65401a
Chandan Giri <jpgiri017@gmail.com>
2022-03-06 00:21:16 +0530

    Time: 12 ms (88.72%), Space: 8.6 MB (72.71%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -6,23 +6,25 @@ public:
         
         int sLen=s.length() , pLen=p.length();
         
+        if(sLen < pLen)
+            return ans;
+        
         int freqP[26]={0};
+        int freqS[26]={0};
+        
         for(int i=0;i<pLen;i++)
         {
             freqP[p[i]-'a']++;
+            freqS[s[i]-'a']++;
         }
+        
+        freqS[s[pLen-1]-'a']--;
         
         for(int i=0;i<sLen-pLen+1;i++)
         {
-            int freqS[26]={0};
-            for(int j=i;j<i+pLen;j++)
-            {
-                freqS[s[j]-'a']++;
-            }
             
+            freqS[s[i+pLen-1]-'a']++;
             bool flag=true;
-            
-           
             for(int i=0;i<26;i++)
             {
                 if(freqP[i]!=freqS[i])
@@ -34,6 +36,9 @@ public:
             
             if(flag)
                 ans.push_back(i);
+            
+            freqS[s[i]-'a']--;
+            
            
         }
         
