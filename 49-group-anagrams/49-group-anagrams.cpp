136f4663e7b2f1f10da3ae0ba4aa4e513f3ddd22
Chandan Giri <jpgiri017@gmail.com>
2022-03-05 13:59:53 +0530

    Time: 167 ms (8.18%), Space: 46.7 MB (5.01%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    map<char,int> getHash(string &s)
+    {
+        map<char,int> mpp;
+        
+        for(auto it:s)
+        {
+            mpp[it]++;
+        }
+        
+        return mpp;
+    }
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        map<map<char,int> , vector<string>> mpp;
+        vector<vector<string>> ans;
+        
+        int n=strs.size();
+        for(int i=0;i<n;i++)
+        {
+            map<char , int> temp=getHash(strs[i]);
+            
+            mpp[temp].push_back(strs[i]);
+        }
+        
+        
+        for(auto it:mpp)
+        {
+            ans.push_back(it.second);
+        }
+        
+        cout<<mpp.size()<<endl;
+        
+        return ans;
+        
+    }
+};
