da6cce967cffb90d100ffcbace8033fc58ddce63
Chandan Giri <jpgiri017@gmail.com>
2022-03-13 18:19:22 +0530

    Time: 255 ms (69.46%), Space: 84.4 MB (26.36%) - LeetHub

---
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -0,0 +1,44 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    int ind=-1;
+    int prev=-1;
+    StockSpanner() {
+        
+        
+    }
+    
+    int next(int price) {
+        
+        ind++;
+        if(prev!=-1)
+            st.push({prev , ind-1});
+        
+        prev=price;
+        if(st.size()==0)
+            return (ind-(-1));
+        else if(st.size()>0 && st.top().first>price)
+            return (ind-(st.top().second));
+        else if(st.size()>0 && st.top().first<=price)
+        {
+            while(st.size()> 0 && st.top().first<=price)
+                st.pop();
+            
+            if(st.size()==0)
+                return (ind-(-1));
+            else
+                return (ind-(st.top().second));
+        }
+        
+        // isko isliye likha hai taaki ye non void function should return a value ka error na de
+        return 1;
+        
+        
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
