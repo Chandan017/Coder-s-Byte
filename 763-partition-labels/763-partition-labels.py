a8eeaf6dc856ba8357e5bf851fdbc225f03b5a45
Chandan Giri <jpgiri017@gmail.com>
2022-03-21 13:40:22 +0530

    Time: 38 ms (93.12%), Space: 13.9 MB (78.97%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def partitionLabels(self, S):
+        result, last_seen, max_last_seen, count = [], {}, 0, 0
+        for i, char in enumerate(S):
+            last_seen[char] = i
+        for i, char in enumerate(S):
+            max_last_seen = max(max_last_seen, last_seen[char])
+            count += 1
+            if i == max_last_seen:
+                result.append(count)
+                count = 0
+        return result
+        
