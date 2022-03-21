681c6c03931c3942c947eb80b842e1a759752001
Chandan Giri <jpgiri017@gmail.com>
2022-03-22 00:15:52 +0530

    Time: 5 ms (86.94%), Space: 7.1 MB (98.29%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    bool check(int map1[] , int map2[] , string &s1)
+    bool check(int map1[] , int map2[])
     {
         for(int i=0;i<26;i++)
         {
-            if(map1[i]!=0)
-            {
-                //cout<<i<<" ";
-                if(map1[i]!=map2[i])
-                    return false;
-            }
+            if(map1[i]!=map2[i])
+                return false;
         }
-      //  cout<<endl;
         return true;
     }
    
@@ -29,32 +24,15 @@ public:
              map1[s1[i]-'a']++;
              map2[s2[i]-'a']++;
         }
-       
-        
-        
         map2[s2[n-1]-'a']--;
            
-        for(int i=0;i<26;i++)
-        {
-            cout<<map1[i];
-        }
-        cout<<endl;
-         for(int i=0;i<26;i++)
-        {
-            cout<<map2[i];
-        }
-        cout<<endl;
         for(int i=0;i<m-n+1;i++)
         {
             map2[s2[i+n-1]-'a']++;
-            bool flag=check(map1,map2,s1);
+            bool flag=check(map1,map2);
             
-            if(flag==true)
-            {
-                //cout<<i<<endl;
+            if(flag)
                 return true;
-            }
-        
             
             map2[s2[i]-'a']--;
             
