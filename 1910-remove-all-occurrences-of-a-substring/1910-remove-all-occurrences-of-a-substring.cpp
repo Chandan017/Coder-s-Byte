2a5abf9d3042bffdaaeae951f2581c01af109c67
Chandan Giri <jpgiri017@gmail.com>
2022-03-21 23:14:58 +0530

    Time: 7 ms (41.61%), Space: 6.6 MB (86.04%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        
+       
+        while(s.length() >0 && s.find(part)!=string::npos)
+        {
+            s.erase(s.find(part) , part.length());
+        }
+        
+        return s;
+        
+    }
+};
