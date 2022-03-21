5316eab026c7aeec53a6a69ee5405eaa957a48ad
Chandan Giri <jpgiri017@gmail.com>
2022-03-21 19:35:28 +0530

    Time: 13 ms (18.22%), Space: 7.9 MB (7.24%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,44 +1,33 @@
 class Solution {
-    
-    bool valid(char ch){
-        if( (ch>='a' && ch<='z') || (ch>='A' && ch <= 'Z') || (ch>='0' && ch<='9'))
-            return 1;
-        
-        else return 0;
-    }
-    
-    char toLowerCase(char a){
-        if ( (a>='a' && a<='z') || (a>='0' && a<='9'))
-            return a;
-        else return a-'A'+'a';
-    }
-    
-    bool checkPalinedrome(string s){
-        int i=0,j=s.length()-1;
-        while (i<j){
-            if (s[i] != s[j])
-                return 0;
-            i++;
-            j--;
-        }
-        return 1;
-    }
-    
 public:
+    
+    bool isValid(char c)
+    {
+        return ((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9'));
+    }
+    
     bool isPalindrome(string s) {
-        int n=s.size();
-       string temp="";
-        for(int i=0;i<n;i++)
-            if(valid(s[i]))
-               temp.push_back(s[i]);
         
-      
-       
-        for(int i=0;i<temp.size();i++)
-          temp[i]=toLowerCase(temp[i]);
-        cout<<temp;
+        string temp="";
         
-       return checkPalinedrome(temp);
-
+        for(int i=0;i<s.length();i++)
+        {
+            if(isValid(s[i]))
+            {
+                if(isupper(s[i]))
+                    s[i]=tolower(s[i]);
+                temp+=s[i];
+            }
+        }
+        
+        cout<<temp<<endl;
+        string dummy=temp;
+        
+        reverse(dummy.begin() , dummy.end());
+        
+        return (temp==dummy);
+        
+        
+        
     }
 };
