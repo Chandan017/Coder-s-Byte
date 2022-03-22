a6025c035a8511830660d5ce8e80ed121ba745e9
Chandan Giri <jpgiri017@gmail.com>
2022-03-22 20:02:29 +0530

    Time: 290 ms (55.38%), Space: 84.3 MB (62.97%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,6 +1,5 @@
 class StockSpanner {
 public:
-    
     stack<pair<int,int>> st;
     int ind=-1;
     int prev=-1;
@@ -10,21 +9,16 @@ public:
     
     int next(int price) {
         
-        
         ind++;
         if(prev!=-1)
         {
             st.push({prev,ind-1});
         }
         prev=price;
-        
         if(st.size()==0)
-            return (ind-(-1));
-        if(st.size()>0 && st.top().first>price)
-        {
+            return 1;
+        if(st.size() >0 && st.top().first>price)
             return (ind-(st.top().second));
-        }
-        
         else if(st.size()>0 && st.top().first<=price)
         {
             while(st.size()>0 && st.top().first<=price)
@@ -37,6 +31,7 @@ public:
         }
         
         return 1;
+        
     }
 };
 
