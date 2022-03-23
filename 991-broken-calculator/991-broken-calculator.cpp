28af5230d06e411bb54557deb06c346bcc31bd67
Chandan Giri <jpgiri017@gmail.com>
2022-03-24 00:45:32 +0530

    Time: 4 ms (27.86%), Space: 5.9 MB (73.18%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        
+        if(startValue >=target)
+            return startValue-target;
+        
+        if(target%2==0)
+            return 1+brokenCalc(startValue , target/2);
+        else
+            return 1+brokenCalc(startValue , target+1);
+        
+    }
+};
