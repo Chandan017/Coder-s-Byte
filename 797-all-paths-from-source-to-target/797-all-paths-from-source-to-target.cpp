e19e76018a23af07ea1031406fee2e2d6388bff6
Chandan Giri <jpgiri017@gmail.com>
2022-03-24 10:42:57 +0530

    Time: 22 ms (56.21%), Space: 17.1 MB (19.43%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -3,52 +3,36 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         
         int n=graph.size();
-        vector<vector<int>> ans;
-        
-        vector<int> adj[n];
-        
-        for(int i=0;i<n;i++)
-        {
-            for(auto &it:graph[i])
-                adj[i].push_back(it);
-        }
-        
-        
-        
-        vector<int> temp;
-        temp.push_back(0);
         
         queue<pair<int,vector<int>>> q;
-        q.push({0,temp});
+        vector<int> temp;
+        temp.push_back(0);
+        q.push({0 , temp});
         
+        vector<vector<int>> ans;
         
         while(!q.empty())
         {
-            int ind=q.front().first;
-            vector<int> temp=q.front().second;
+            int node=q.front().first;
+            vector<int> path=q.front().second;
             q.pop();
             
-            if(ind==n-1)
+            if(node==n-1)
             {
-                ans.push_back(temp);
+                ans.push_back(path);
+                continue;
             }
             
-            for(auto &it:adj[ind])
+            for(auto it:graph[node])
             {
-                temp.push_back(it);
-                q.push({it,temp});
-                temp.pop_back();
+                path.push_back(it);
+                q.push({it , path});
+                path.pop_back();
             }
+            
         }
         
-        
-        
-        
-        
-        
-        
         return ans;
-        
         
     }
 };
