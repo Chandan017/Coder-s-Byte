4e2decb1200128c96d32200a394e28434270f8d4
Chandan Giri <jpgiri017@gmail.com>
2022-03-28 23:47:20 +0530

    Time: 5 ms (20.50%), Space: 6.1 MB (50.63%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string baseNeg2(int n) {
+        
+        string ans="";
+        if(n==0)
+            return (to_string(n));
+        while(n)
+        {
+            int rem=n%(-2);
+            n/=(-2);
+            
+            if(rem<0)
+            {
+                rem+=2;
+                n+=1;
+            }
+            ans+=(to_string(abs(rem)));
+        }
+        reverse(ans.begin() , ans.end());
+        return ans;
+        
+    }
+};
