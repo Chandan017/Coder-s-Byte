87114b9b7b0045e48b78396644a7a71a37e4bfed
Chandan Giri <jpgiri017@gmail.com>
2022-04-04 23:14:08 +0530

    Time: 107 ms (71.62%), Space: 37.8 MB (99.44%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -17,32 +17,31 @@ public:
         ListNode* prevptr=NULL;
         ListNode* nextptr;
         ListNode* currptr=head;
-        
+        int n=0;
         while(currptr)
         {
             nextptr=currptr->next;
             currptr->next=prevptr;
             prevptr=currptr;
             currptr=nextptr;
+            n++;
         }
         
         head=prevptr;
         stack<int> st;
-        vector<int> ans;
+        vector<int> ans(n,0);
+        int currInd=0;
         while(head)
         {
             while(st.size()>0 && st.top()<=head->val)
                 st.pop();
-            if(st.size()==0)
-                ans.push_back(0);
-            else
-                ans.push_back(st.top());
+            if(st.size() > 0)
+                ans[n-currInd-1] =st.top();
             
             st.push(head->val);
             head=head->next;
+            currInd++;
         }
-        
-        reverse(ans.begin() , ans.end());
         return ans;
         
         
