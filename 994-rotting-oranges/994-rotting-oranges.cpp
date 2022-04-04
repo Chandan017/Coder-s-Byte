a5483728065962b039562e09e67f5e2ac36390c9
Chandan Giri <jpgiri017@gmail.com>
2022-04-05 01:55:17 +0530

    Time: 18 ms (16.40%), Space: 13.2 MB (49.73%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -2,59 +2,49 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         
-        queue<pair<int,pair<int,int>>> q;
-        int totalOranges=0;
         int n=grid.size();
         int m=grid[0].size();
-        
+        queue<pair<int,pair<int,int>>> q;
+        int totalOranges=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(grid[i][j]==2)
-                {
                     q.push({0 , {i,j}});
-                }
-                
                 if(grid[i][j]!=0)
                     totalOranges++;
             }
         }
-        
-        int dx[]={-1,1,0,0};
-        int dy[]={0,0,-1,1};
-        
-        int rottenOranges=0;
-        int maxTime=0;
+        int dx[]={-1 , 1 , 0 ,0};
+        int dy[]={0 , 0 , -1 , 1};
+        int rottenOranges=0 , maxTime=0;
         while(!q.empty())
         {
-            
-            int time=q.front().first;
+            int currTime=q.front().first;
             int x=q.front().second.first;
             int y=q.front().second.second;
             q.pop();
             
-            maxTime=max(maxTime,time);
+            maxTime=max(maxTime , currTime);
             
             for(int ind=0;ind<4;ind++)
             {
-                int newX=x+dx[ind];
-                int newY=y+dy[ind];
+                int newX = x + dx[ind];
+                int newY = y + dy[ind];
                 
-                if(newX>=0 && newX<n && newY>=0 && newY<m && grid[newX][newY]==1)
+                if(newX>=0 && newY>=0 && newX<n && newY<m && grid[newX][newY]==1)
                 {
-                    grid[newX][newY]=2;
-                    q.push({time+1 , {newX,newY}});
+                    grid[newX][newY] = 2;
+                    q.push({currTime+1 , {newX , newY}});
                 }
             }
-            
             rottenOranges++;
-            
         }
         
-        if(rottenOranges==totalOranges)
+        if(rottenOranges == totalOranges)
             return maxTime;
-        return -1;
-        
+        else
+            return -1;
     }
 };
