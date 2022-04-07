3a65710930a24b45244641c66c531bc5c6730c64
Chandan Giri <jpgiri017@gmail.com>
2022-04-08 02:28:51 +0530

    Time: 856 ms (59.12%), Space: 129.5 MB (53.52%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -1,31 +1,48 @@
 class Solution {
 public:
+
+// class CPU{
+// public:
+//     int processTime ;
+//     int index;
+    
+//     CPU(int processTime , int index)
+//     {
+//         this->processTime = processTime ;
+//         this->index = index;
+//     }
+// };
     vector<int> getOrder(vector<vector<int>>& tasks) {
         
-        int n=tasks.size();
-        
+        int n = tasks.size();
+        vector<int> ans;
         
         for(int i=0;i<n;i++)
             tasks[i].push_back(i);
         
         sort(tasks.begin() , tasks.end());
         
-        long currTime = 0;
+        long currTime = tasks[0][0];
         
-        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>> > pq;
+        cout<<currTime<<endl;
+        int ind = 0;
+        priority_queue<pair<int,int> , vector<pair<int,int>> , greater<pair<int,int>>> pq;
         
-        int ind =0;
-        vector<int> ans;
-       
-        while( ind < n || !pq.empty())
+        while(ind < n || pq.size() > 0)
         {
-           if(pq.size()==0 )
-               currTime = max (currTime  , (long)tasks[ind][0]);
-               
+            
+            if(pq.size()==0)
+                currTime = max(currTime , (long)tasks[ind][0]);
+            
             
             while(ind < n && tasks[ind][0] <= currTime)
             {
-                pq.push({tasks[ind][1] , tasks[ind][2]});
+                int time = tasks[ind][1];
+                int index = tasks[ind][2];
+                
+                // CPU temp(time , index);
+                pq.push({time , index});
+                
                 ind++;
             }
             
@@ -33,9 +50,11 @@ public:
             int index = pq.top().second;
             pq.pop();
             
-            currTime += processTime;
+            currTime += processTime ;
             
             ans.push_back(index);
+            
+            
         }
         
         return ans;
