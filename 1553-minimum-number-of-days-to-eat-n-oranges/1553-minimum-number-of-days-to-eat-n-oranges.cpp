80c20e397493308a7ba9f9cff13011d432535ba6
Chandan Giri <jpgiri017@gmail.com>
2022-04-11 21:44:22 +0530

    Time: 1339 ms (5.28%), Space: 97 MB (7.62%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    
+    int getMin(int n)
+    {
+        queue<int> q;
+        
+        q.push(n);
+        int days = 0;
+        while(!q.empty())
+        {
+            int size = q.size();
+            set<int> st;
+            while(size--)
+            {
+                int leftOranges = q.front();
+                q.pop();
+                     
+                if(leftOranges < 0)
+                    continue;
+                if(leftOranges==0)
+                    return days;
+            
+                q.push(leftOranges - 1);
+            
+                if(leftOranges%2==0)
+                {
+                    int canEat = leftOranges / 2;
+                
+                    st.insert(leftOranges - canEat);
+                }
+            
+                if(leftOranges % 3==0)
+                {
+                    int canEat = 2 * (leftOranges / 3);
+                
+                    st.insert(leftOranges - canEat );
+                }
+            }
+            
+            for(auto &ele:st)
+                q.push(ele);
+            
+            days++;
+            
+            
+        }
+        
+        return days;
+    }
+    int minDays(int n) {
+        
+        int minDays = getMin(n);
+        
+        return minDays;
+        
+    }
+};
