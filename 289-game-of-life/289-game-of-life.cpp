41fce2860087017ab9c0c956a9f81b5619ed7e4c
Chandan Giri <jpgiri017@gmail.com>
2022-04-13 00:58:20 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (9.40%) - LeetHub

---
+++ 289-game-of-life/289-game-of-life.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        
+        
+        int n=board.size();
+        int m=board[0].size();
+        
+        vector<vector<int>> ans(n,vector<int> (m,0));
+        
+        int dx[]={-1,1,0,0,-1,-1,1,1};
+        int dy[]={0,0,-1,1,-1,1,-1,1};
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                int liveCnt = 0;
+                
+                for(int ind =0;ind<8;ind++)
+                {
+                    int newX = i + dx[ind];
+                    int newY = j + dy[ind];
+                    
+                    if(newX >=0 && newY>=0 && newX<n && newY<m)
+                    {
+                        if(board[newX][newY] == 1)
+                            liveCnt++;
+                    }
+                }
+                
+                if(board[i][j]==1)
+                {
+                    if(liveCnt < 2)
+                        ans[i][j] = 0;
+                    else if(liveCnt == 2 || liveCnt == 3)
+                        ans[i][j] = 1;
+                    else if(liveCnt > 3)
+                        ans[i][j] = 0;
+                }
+                else if (board[i][j]==0)
+                {
+                    if(liveCnt==3)
+                        ans[i][j] = 1;
+                }
+                
+                
+                
+                
+                
+            }
+        }
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+                board[i][j] = ans[i][j];
+        }
+        
+    }
+};
