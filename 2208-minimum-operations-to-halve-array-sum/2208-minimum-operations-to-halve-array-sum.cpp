11bb7758b1ca899eab3889c71c4f7576fe952f1f
Chandan Giri <jpgiri017@gmail.com>
2022-04-21 23:29:11 +0530

    Time: 495 ms (17.50%), Space: 89.4 MB (20.86%) - LeetHub

---
+++ 2208-minimum-operations-to-halve-array-sum/2208-minimum-operations-to-halve-array-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int halveArray(vector<int>& nums) {
+        
+        double totalSum = 0.00;
+        priority_queue<double> pq;
+        for(auto it:nums)
+        {
+            totalSum += it;
+            pq.push(it);
+        }
+        
+        int cnt = 0;
+        
+        double halfSum = totalSum / 2.00;
+        double reducedSum = 0.00;
+        while(reducedSum < halfSum)
+        {
+            double val = pq.top();
+            pq.pop();
+            val = val / 2.00;
+            
+            reducedSum += val;
+            pq.push(val);
+            
+            cnt++;
+        }
+        
+        cout<<halfSum<<endl;
+        
+        return cnt;
+    }
+};
