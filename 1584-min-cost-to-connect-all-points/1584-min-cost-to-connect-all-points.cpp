db96e615ce6a03c2e8c8367bd3bae89b30de5c0f
Chandan Giri <jpgiri017@gmail.com>
2022-04-26 20:48:48 +0530

    Time: 1396 ms (17.11%), Space: 177.1 MB (12.20%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    
+    int getParent(vector<int> &parent , int node)
+    {
+        if(parent[node] == node)
+            return node;
+        
+        parent[node] = getParent(parent , parent[parent[node]]);
+        
+        return parent[node];
+    }
+    
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        
+        int n = points.size();
+        vector<int> parent(n);
+        iota(parent.begin() , parent.end() , 0);
+        
+        int edges = 0;
+        int ans  = 0;
+        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>> > pq;
+        
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j = i+1 ;j<n;j++)
+            {
+                int dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                
+                pq.push({dist , i , j});
+            }
+        }
+        
+        while(edges != n-1)
+        {
+            vector<int> temp = pq.top();
+            pq.pop();
+            
+            int p1 = getParent(parent , temp[1]);
+            int p2 = getParent(parent , temp[2]);
+            
+            if(p1 != p2)
+            {
+                parent[p1] = p2;
+                
+                ans += temp[0];
+                edges++;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
