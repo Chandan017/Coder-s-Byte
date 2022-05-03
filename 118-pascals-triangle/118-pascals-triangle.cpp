21734b577bdbf711f03590e4ebce7c59b7c65a59
Chandan Giri <jpgiri017@gmail.com>
2022-05-03 23:11:08 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.70%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,38 +1,17 @@
 class Solution {
 public:
-    
-    int dp[31][31];
-    
-    int get(int r , int c)
-    {
-        if(r<0 || c<0 || c > r)
-            return 0;
-        
-        if(r==0 && c==0)
-            return 1;
-        
-        if(dp[r][c] )
-            return dp[r][c];
-        
-        dp[r][c] = get(r-1 , c-1) + get(r-1 , c);
-        
-        return dp[r][c];
-        
-        
-        
-    }
+   
     vector<vector<int>> generate(int numRows) {
         
         vector<vector<int>> ans;
         
         for(int i=0;i<numRows;i++)
         {
-            vector<int> temp;
+            vector<int> temp(i+1 , 1);
             
-            for(int j=0;j<=i;j++)
+            for(int j=1;j<i;j++)
             {
-                int val = get(i , j);
-                temp.push_back(val);
+                temp[j] = ans[i-1][j-1] + ans[i-1][j];
             }
             
             ans.push_back(temp);
