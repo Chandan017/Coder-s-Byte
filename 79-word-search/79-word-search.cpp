167d8a13c93095702f34f59d1e279e725e61fbf8
Chandan Giri <jpgiri017@gmail.com>
2022-05-06 01:30:04 +0530

    Time: 412 ms (61.26%), Space: 8 MB (37.29%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int n, m;
+    
+    bool check(vector<vector<char>> &board , int r , int c , string &word)
+    {
+        if(word.size()==0)
+            return true;
+        
+        if(r<0 || c<0 || r>=n || c>=m || board[r][c] != word[0])
+            return false;
+        
+        char val = board[r][c];
+        int dx[]={-1,1,0,0};
+        int dy[]={0,0,-1,1};
+        
+        board[r][c] = '*';
+        string s = word.substr(1);
+        for(int ind=0;ind<4;ind++)
+        {
+            int newR = r+dx[ind];
+            int newC = c+dy[ind];
+            
+            if(check(board , newR , newC , s))
+                return true;
+        }
+        
+        board[r][c] = val;
+        
+        return false;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        n = board.size();
+        m = board[0].size();
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(check(board , i , j , word))
+                    return true;
+            }
+        }
+        
+        return false;
+        
+    }
+};
