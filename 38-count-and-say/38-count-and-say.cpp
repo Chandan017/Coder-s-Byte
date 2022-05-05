50a8e3ab01516dc90e326924f61ad0a552a22767
Chandan Giri <jpgiri017@gmail.com>
2022-05-06 01:03:11 +0530

    Time: 4 ms (88.71%), Space: 6.7 MB (57.70%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,39 +1,33 @@
 class Solution {
 public:
     
-    string get(int n)
-    {
-        if(n==1)
-            return "1";
-        
-        int times = 0;
-        
-        string temp = get(n-1);
-        temp += '0';
-        char val = temp[0];
-    
-        string ans = "";
-        
-        for(auto it:temp)
-        {
-            if(it==val)
-                times++;
-            else
-            {
-                ans += (times+'0');
-                ans += val;
-                val = it;
-                times = 1;
-            }
-        }
-    
-        return ans;
-    }
-    
+   
     string countAndSay(int n) {
         
-        string ans = get(n);
+        string ans = "1";
         
+        for(int i=2;i<=n;i++)
+        {
+            string dummy = "";
+            char val = ans[0];
+            int times = 0;
+            ans += '0';
+            
+            for(auto it:ans)
+            {
+                if(it==val)
+                    times++;
+                else
+                {
+                    dummy += (times + '0');
+                    dummy += val;
+                    val = it;
+                    times = 1;
+                }
+            }
+            
+            ans = dummy;
+        }
         return ans;
         
     }
