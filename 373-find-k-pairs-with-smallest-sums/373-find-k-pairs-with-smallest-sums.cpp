2287cf700048e9a5e52d4508e4ee15bb8d27f800
Chandan Giri <jpgiri017@gmail.com>
2022-05-08 01:18:44 +0530

    Time: 558 ms (24.89%), Space: 122.9 MB (27.39%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         
-        priority_queue<pair<int,pair<int,int>>> pq;
+        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> pq;
         vector<vector<int>> ans;
-        
-        for(auto num1 :nums1)
+        int n1 = nums1.size() , n2 = nums2.size();
+        pq.push({nums1[0] + nums2[0] , 0,0});
+        set<pair<int,int>> vis;
+        while(pq.size() && k-- >0)
         {
-            for(auto num2:nums2)
-            {
-                int sum = num1 + num2;
-                
-                if(pq.size() < k)
-                    pq.push({sum , {num1 , num2}});
-                else if(sum < pq.top().first)
-                {
-                    pq.pop();
-                    pq.push({sum , {num1 , num2}});
-                }
-                else
-                    break;
-            }
-        }
-        
-        while(pq.size())
-        {
-            ans.push_back({pq.top().second.first , pq.top().second.second});
+            vector<int> temp = pq.top();
             pq.pop();
+            
+            ans.push_back({nums1[temp[1]] , nums2[temp[2]]});
+            
+            if(temp[1]+1 < n1 && vis.find({temp[1]+1 , temp[2]}) == vis.end())
+            {
+                 pq.push({nums1[temp[1]+1] + nums2[temp[2]] , temp[1]+1 , temp[2]});
+                vis.insert({temp[1]+1 , temp[2]});
+            }
+               
+            
+            if(temp[2]+1 < n2 && vis.find({temp[1] , temp[2]+1}) == vis.end())
+            {
+                pq.push({nums1[temp[1]]+nums2[temp[2]+1] , temp[1] , temp[2]+1});
+                vis.insert({temp[1] , temp[2]+1});
+            }
+                
         }
         
         return ans;
