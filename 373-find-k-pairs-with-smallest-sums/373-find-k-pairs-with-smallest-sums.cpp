3b6a215f31ba47e0c5b36c9b194c449d06891477
Chandan Giri <jpgiri017@gmail.com>
2022-05-08 01:32:20 +0530

    Time: 355 ms (63.91%), Space: 106.3 MB (96.71%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -8,7 +8,6 @@ public:
         vector<vector<int>> ans;
         int n1 = nums1.size() , n2 = nums2.size();
         
-        set<pair<int,int>> vis;
         
         pq.push({nums1[0] + nums2[0] , {0,0}});
         
@@ -20,18 +19,16 @@ public:
             
             ans.push_back({nums1[i] , nums2[j]});
             
-            if(i+1 < n1 && vis.find({i+1 , j}) == vis.end())
+            if(i+1 < n1 )
             {
                 int sum = nums1[i+1] + nums2[j];
                 pq.push({sum , {i+1 , j}});
-                vis.insert({i+1 , j});
             }
             
-            if(j+1 < n2 && vis.find({i , j+1}) == vis.end())
+            if(i==0 && j+1 < n2)
             {
                 int sum = nums1[i] + nums2[j+1];
                 pq.push({sum , {i , j+1}});
-                vis.insert({i ,j+1});
             }
         }
         
