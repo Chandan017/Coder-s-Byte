af2988d316c09b1daa37a2503edd1e514658e17c
Chandan Giri <jpgiri017@gmail.com>
2022-05-08 09:48:26 +0530

    Time: 530 ms (25.00%), Space: 26.3 MB (75.00%) - LeetHub

---
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+   
+    int n , m;
+    int dp[101][101][300];	
+    bool check(vector<vector<char>> &grid , int r , int c , int sum)
+    {
+        if(r>=n || c>=m )
+            return false;
+      
+        if(grid[r][c] == ')' && sum == 0)
+            return false;
+        
+        if(grid[r][c] == '(')
+            sum++;
+        else
+            sum--;
+        if(r==n-1 && c==m-1)
+        {
+            cout<<sum<<endl;
+            return (sum==0);
+        }
+        
+        if(dp[r][c][sum] != -1)
+            return dp[r][c][sum];
+        
+        bool down = check(grid , r+1 , c, sum);
+        bool right = check(grid , r , c+1 , sum);
+        
+        return dp[r][c][sum] = (down || right);
+        
+        
+    }
+    
+    bool hasValidPath(vector<vector<char>>& grid) {
+        
+        
+        n = grid.size() , m = grid[0].size();
+         memset(dp, -1, sizeof(dp));
+        
+        bool ans = check(grid , 0 , 0 , 0);
+        
+        return ans;
+        
+       
+        
+    }
+};
