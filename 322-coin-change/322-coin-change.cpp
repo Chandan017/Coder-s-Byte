61a2b4a71e1676dd11a65b5e946bf2a13abca955
Chandan Giri <jpgiri017@gmail.com>
2022-05-14 00:56:39 +0530

    Time: 73 ms (85.82%), Space: 10.7 MB (79.99%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    
-    long dp[10001][13];
-    long get(vector<int> &coins , int amount , int n)
+    int dp[10001][13];
+    int get(vector<int> &coins , int n , int amount)
     {
-        
         if(amount == 0)
             return 0;
         
         if(n==0)
-            return INT_MAX -1 ;
+            return INT_MAX -1;
         
         if(dp[amount][n])
             return dp[amount][n];
-            
-      
-        int cnt;
         
+        int cnt;
         if(coins[n-1] > amount)
-            cnt = get(coins , amount , n-1);
+            cnt = get(coins , n-1 , amount);
         
         else
         {
-            int takeIt = 1 + get(coins , amount - coins[n-1] , n);
-            int notTakeIt = get(coins , amount , n-1);
+            int takeIt = 1 + get(coins , n , amount - coins[n-1]);
+            int notTakeIt = get(coins , n-1 , amount);
             
             cnt = min(takeIt , notTakeIt);
         }
@@ -31,13 +27,17 @@ public:
         return dp[amount][n] = cnt;
         
     }
+    
     int coinChange(vector<int>& coins, int amount) {
-     
         
-        int ans = get(coins , amount , coins.size());
+        int n = coins.size();
         
-        if(ans == INT_MAX - 1)
+        int ans = get(coins , n , amount);
+        
+        if(ans == INT_MAX -1)
             ans = -1;
+        
         return ans;
+        
     }
 };
