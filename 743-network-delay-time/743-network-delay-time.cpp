fd99d917a28b4626ec33dd854013d46ddf249d22
Chandan Giri <jpgiri017@gmail.com>
2022-05-15 00:21:01 +0530

    Time: 152 ms (60.12%), Space: 43.4 MB (41.47%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -2,44 +2,56 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         
-        vector<pair<int , int>> adj[n+1];
+        vector<pair<int,int>> adj[n+1];
         
-        for(auto &it:times)
+        for(auto it:times)
         {
-            int u = it[0] , v = it[1] , t = it[2];
+            int u = it[0] , v = it[1] , wt = it[2];
             
-            adj[u].push_back({v , t});
+            adj[u].push_back({v , wt});
         }
+        vector<int> dist(n+1 , INT_MAX);
+        dist[k] = 0;
+        queue<pair<int,int>> q;
+        q.push({k,0});
         
-        queue<int> q;
-        q.push(k);
-        int maxTime = -1 ;
-        vector<int> time(n+1 , INT_MAX);
-        time[k] = 0;
+        int nodesCovered = 0;
+        int maxTime = 0;
+        
         while(!q.empty())
         {
-            int currNode = q.front();
-            q.pop();
-        
-            int currTime = time[currNode];
-            for(auto &it:adj[currNode])
+            int len = q.size();
+            
+            while(len--)
             {
-                if(time[it.first] > currTime + it.second)
+                int node = q.front().first;
+                int currDist = q.front().second;
+                q.pop();
+                
+            
+                for(auto it:adj[node])
                 {
-                    q.push(it.first);
-                    time[it.first] = currTime + it.second;
+                    int newDist = currDist + it.second;
+                    
+                    if(dist[it.first] > newDist)
+                    {
+                        dist[it.first] = newDist;
+                        q.push({it.first , newDist});
+                    }
                 }
+                    
                 
             }
         }
-              
-        for(int i=1;i<n+1;i++)
-        {
-            if(time[i]==INT_MAX)
-                return -1;
-            maxTime = max(maxTime , time[i]);
-        }
         
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(dist[i]==INT_MAX)
+                return -1;
+            else
+                maxTime = max(maxTime , dist[i]);
+        }
         
         return maxTime;
         
