7070e9194f8d415b01e1f48a54558ff5b7b414a1
Chandan Giri <jpgiri017@gmail.com>
2022-05-22 16:42:38 +0530

    Time: 801 ms (5.05%), Space: 6.9 MB (87.55%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        
+        int n = s.length();
+        int maxLen =0;
+        for(int i=0;i<n;i++)
+        {
+            int cnt =0;
+            for(int j=i;j<n;j++)
+            {
+                if(s[j]=='(')
+                    cnt++;
+                else
+                    cnt--;
+                
+                if(cnt <0)
+                    break;
+                
+                if(cnt==0)
+                {
+                    int currLen = j-i+1;
+                    
+                    maxLen = max(maxLen , currLen);
+                }
+            }
+        }
+        
+        return maxLen;
+        
+    }
+};
