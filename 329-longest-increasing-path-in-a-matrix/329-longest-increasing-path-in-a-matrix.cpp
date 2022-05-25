49e3fb974bfb77f63222325bb3d1e3ca2f873a01
Chandan Giri <jpgiri017@gmail.com>
2022-05-25 22:29:59 +0530

    Time: 74 ms (41.63%), Space: 16.1 MB (51.59%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,35 +1,52 @@
 class Solution {
+
+private:
+    int n , m;
+    int dx[4]={-1,1,0,0};
+    int dy[4]={0,0,-1,1};
+    
+    int solve(vector<vector<int>> &matrix , int r , int c , vector<vector<int>> &dp)
+    {
+        
+        if(dp[r][c])
+            return dp[r][c];
+        
+        int maxPath = 1;
+        for(int ind=0;ind<4;ind++)
+        {
+            int newR = r+dx[ind];
+            int newC = c+dy[ind];
+            
+            if(newR>=0 && newC>=0 && newR<n && newC<m && matrix[newR][newC] > matrix[r][c])
+            {
+                int currPath = 1 + solve(matrix , newR , newC , dp);
+                maxPath = max(maxPath , currPath);
+            }
+        }
+        
+        return dp[r][c] = maxPath;
+    }
 public:
     
-    
-int moves[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
-vector<vector<int> > dp; 
-int maxPath, n, m;
-    
-int solve(vector<vector<int>>& mat, int i, int j)
-{
-	if(dp[i][j]) return dp[i][j]; 
-	dp[i][j] = 1;  
-	
-	for(int k = 0; k < 4; k++){ 
-		int new_i = i + moves[k][0], new_j = j + moves[k][1];
-	
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
         
-		if(new_i < 0 || new_j < 0 || new_i >= n || new_j >= m || mat[new_i][new_j] <= mat[i][j]) continue;
-		
+        n = matrix.size() , m= matrix[0].size();
+        vector<vector<int>> dp(n+1 , vector<int> (m+1));
+        int maxPath = 1;
         
-		dp[i][j] = max(dp[i][j], 1 + solve(mat, new_i, new_j));
-	}         
-	return dp[i][j];
-}
-   int longestIncreasingPath(vector<vector<int>>& matrix) {
-	maxPath = 0, n = size(matrix), m = size(matrix[0]);
-	dp.resize(n, vector<int>(m));
-	
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < m; j++)
-			maxPath = max(maxPath, solve(matrix, i, j));            
-	return maxPath;
-}
-
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                int currPath = solve(matrix , i , j , dp);
+                
+                maxPath = max(maxPath , currPath);
+            }
+        }
+        
+        
+        
+        return maxPath;
+        
+    }
 };
