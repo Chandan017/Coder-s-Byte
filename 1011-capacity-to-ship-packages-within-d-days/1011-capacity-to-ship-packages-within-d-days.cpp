c69a2eaf8d362f7cd43396acd55f7ce90b95a5d6
Chandan Giri <jpgiri017@gmail.com>
2022-06-04 17:47:57 +0530

    Time: 85 ms (40.55%), Space: 26 MB (94.68%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    
+    bool solve(vector<int> &nums , int maxAllowed , int days)
+    {
+        int total = 0;
+        int currDays = 1;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            if(total + nums[i] <= maxAllowed)
+                total +=nums[i];
+            else
+            {
+                
+                    currDays++;
+                total = nums[i];
+            }
+            if(currDays > days || nums[i] > maxAllowed)
+                    return false;
+        }
+        
+        return true;
+    }
+    
+    int shipWithinDays(vector<int>& nums, int days) {
+        
+            
+        int low = *max_element(nums.begin() , nums.end());
+        int high = accumulate(nums.begin() , nums.end(),0);
+                
+        int maxMin = high;
+        
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            cout<<mid<<" ";
+            bool isPossible = solve(nums , mid , days);
+            
+            if(isPossible==true)
+            {
+                maxMin = mid;
+                high = mid-1;
+            }
+            else
+                low = mid+1;
+        }
+        
+        return maxMin;
+        
+        
+    }
+};
