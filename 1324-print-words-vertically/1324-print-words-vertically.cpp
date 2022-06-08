374110574e0e58d1101a71dec662ea6dab1ffe72
Chandan Giri <jpgiri017@gmail.com>
2022-06-08 23:27:02 +0530

    Time: 5 ms (15.90%), Space: 6.5 MB (40.16%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<string> printVertically(string s) {
+        
+        int maxLen = 0;
+        int currLen = 0;
+        
+        s += ' ';
+        
+        for(auto &it:s)
+        {
+            if(it==' ')
+            {
+                maxLen = max(maxLen , currLen);
+                currLen = 0;
+            }
+            else
+                currLen += 1;
+        }
+        
+        vector<string> dict;
+        string temp = "";
+        for(auto it:s)
+        {
+            if(it==' ')
+            {
+                while(temp.size() != maxLen)
+                {
+                    temp += ' ';
+                }
+                
+                dict.push_back(temp);
+                temp = "";
+            }
+            else
+                temp += it;
+        }
+        
+        int ind = 0;
+        
+        vector<string> ans;
+        
+        while(ind < maxLen)
+        {
+            string temp = "";
+            for(auto &it:dict)
+            {
+                temp += it[ind];
+            }
+            
+            while(temp.back() == ' ')
+                temp.pop_back();
+            
+            ans.push_back(temp);
+            ind++;
+        }
+        
+        return ans;
+        
+        
+    }
+};
