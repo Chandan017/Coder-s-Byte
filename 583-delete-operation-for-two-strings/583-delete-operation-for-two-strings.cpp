18a43203156843b1ab9fac73fde7e7bec2bc1955
Chandan Giri <jpgiri017@gmail.com>
2022-06-14 18:28:39 +0530

    Time: 44 ms (26.88%), Space: 12.3 MB (34.50%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,28 +1,25 @@
 class Solution {
-public:
-    
-    int lcs(string &word1 , string &word2 , int n , int m , vector<vector<int>> &dp)
-    {
-        if(n==0 || m==0)
-            return 0;
-        
-        if(dp[n][m])
-            return dp[n][m];
-        if(word1[n-1] == word2[m-1])
-            return dp[n][m] = 1 + lcs(word1 , word2 , n-1 , m-1 , dp);
-        return dp[n][m] = max(lcs(word1 , word2 , n-1 , m , dp) , lcs(word1 , word2 , n , m-1 , dp));
-    }
-    
+public:    
     int minDistance(string word1, string word2) {
         
-        if(word1 == word2)
-            return 0;
         int n = word1.length() , m = word2.length();
-        vector<vector<int>> dp(n+1 , vector<int> (m+1));
+        vector<vector<int>> dp(n+1 , vector<int> (m+1 ));
         
-        int longest_common_subSeq = lcs(word1 , word2 , n , m , dp);
-        
-        int deleted_cnt = n + m - (2 * longest_common_subSeq);
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0 || j==0)
+                    dp[i][j] = 0;
+                else if(word1[i-1] == word2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                
+            }
+        }
+                
+        int deleted_cnt = n + m - (2 * dp[n][m]);
         
         return deleted_cnt;
         
