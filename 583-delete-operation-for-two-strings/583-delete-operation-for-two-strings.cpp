7387476363c796b47291fd911b2bd86781b7be21
Chandan Giri <jpgiri017@gmail.com>
2022-06-14 18:24:22 +0530

    Time: 231 ms (9.81%), Space: 12.3 MB (17.03%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    int lcs(string &word1 , string &word2 , int ind1 , int ind2 , vector<vector<int>> &dp)
+    
+    int lcs(string &word1 , string &word2 , int n , int m , vector<vector<int>> &dp)
     {
-        if(ind1==0 || ind2==0)
+        if(n==0 || m==0)
             return 0;
-        if(dp[ind1][ind2])
-            return dp[ind1][ind2];
         
-        if(word1[ind1-1] == word2[ind2-1])
-            return dp[ind1][ind2] = 1 + lcs(word1 , word2 , ind1-1 , ind2-1 , dp);
-        else
-            return dp[ind1][ind2] = max(lcs(word1 , word2 , ind1-1 , ind2 , dp) , lcs(word1 , word2 , ind1 , ind2-1 , dp));
+        if(dp[n][m])
+            return dp[n][m];
+        if(word1[n-1] == word2[m-1])
+            return dp[n][m] = 1 + lcs(word1 , word2 , n-1 , m-1 , dp);
+        return dp[n][m] = max(lcs(word1 , word2 , n-1 , m , dp) , lcs(word1 , word2 , n , m-1 , dp));
     }
     
     int minDistance(string word1, string word2) {
         
+        if(word1 == word2)
+            return 0;
         int n = word1.length() , m = word2.length();
         vector<vector<int>> dp(n+1 , vector<int> (m+1));
         
-        int longestCommonSubsequence = lcs(word1 , word2 , n , m , dp);
+        int longest_common_subSeq = lcs(word1 , word2 , n , m , dp);
         
-        int totalLength = n+m;
+        int deleted_cnt = n + m - (2 * longest_common_subSeq);
         
-        longestCommonSubsequence = totalLength - (2*longestCommonSubsequence);
+        return deleted_cnt;
         
-        
-        return longestCommonSubsequence;
-        
-               
     }
 };
