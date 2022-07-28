7d46e598073747d3f27df942def2d760813b12a7
Chandan Giri <jpgiri017@gmail.com>
2022-07-28 20:01:54 +0530

    Time: 11 ms (20.17%), Space: 11 MB (75.00%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -4,23 +4,22 @@ public:
         
         int low = 0 , high = nums.size()-1;
         
-        while(low<=high)
+        while(low <= high)
         {
-            int mid = (low+high)/2;
-            
+            int mid = (low + high)/2;
             if(nums[mid] == target)
                 return mid;
             
             if(nums[low] <= nums[mid])
             {
-                if(target>=nums[low] && target<nums[mid])
+                if(target >= nums[low] && target < nums[mid])
                     high = mid-1;
                 else
                     low = mid+1;
             }
             else
             {
-                if(target>nums[mid]  && target<=nums[high])
+                if(target > nums[mid] && target <= nums[high])
                     low = mid+1;
                 else
                     high = mid-1;
