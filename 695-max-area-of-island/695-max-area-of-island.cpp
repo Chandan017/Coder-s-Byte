67c02764c7d820478adf7e6c5b8154c6619d7546
Chandan Giri <jpgiri017@gmail.com>
2022-08-05 22:51:26 +0530

    Time: 31 ms (48.49%), Space: 23.2 MB (87.05%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
-    int n,m;
-    int dx[4]={-1,1,0,0};
-    int dy[4]={0,0,-1,1};
+    int n , m;
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
     
-    int getArea(vector<vector<int>> &grid , int r , int c){
+    int getArea(vector<vector<int>> &grid , int r , int c)
+    {
+        if(r<0 || c<0 || r>=n || c>=m || grid[r][c] == 0)
+            return 0;
         
-        queue<pair<int,int>> q;
-        q.push({r,c});
         grid[r][c] = 0;
+        int currArea = 1;
         
-        int area = 0;
-        
-        while(q.size())
+        for(int ind=0;ind<4;ind++)
         {
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
+            int newR = r+dx[ind];
+            int newC = c+dy[ind];
             
-            for(int ind=0;ind<4;ind++)
-            {
-                int newR = r+dx[ind];
-                int newC = c+dy[ind];
-                
-                if(newR>=0 && newC>=0 && newR<n && newC<m && grid[newR][newC]==1)
-                {
-                    q.push({newR , newC});
-                    grid[newR][newC] = 0;
-                }
-            }
-            
-            area++;
+            currArea += getArea(grid , newR , newC);
         }
         
-        
-        return area;
-        
+        return currArea;
     }
-    
-    
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         
-        n = grid.size();
-        m = grid[0].size();
+        n = grid.size() , m = grid[0].size();
+        
         int maxArea = 0;
+        
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
