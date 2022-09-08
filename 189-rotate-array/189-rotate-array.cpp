3b25bacded0ea28e9958a723db5bf3c71ac71e9f
Chandan Giri <jpgiri017@gmail.com>
2022-09-08 16:41:32 +0530

    Time: 45 ms (59.55%), Space: 24.9 MB (83.18%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        k = k%n;
+        
+        reverse(nums.begin() , nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k) , nums.end());
+        reverse(nums.begin() , nums.end());
+        
+    }
+};
