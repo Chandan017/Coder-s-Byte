768c273e560643e7c76ded32f1ac18498d421619
Chandan Giri <jpgiri017@gmail.com>
2022-09-14 00:34:24 +0530

    Time: 31 ms (19.95%), Space: 14 MB (38.65%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        
+        int cnt=0;
+        for(auto it : data)
+        {
+            if(cnt==0) 
+            {
+                
+                if((it>>5) == 0b110) cnt=1; 
+                else if((it>>4) == 0b1110) cnt=2; 
+                else if((it>>3) == 0b11110) cnt=3; 
+                 else if((it>>7)) return false;
+            }
+            
+            else
+            {
+                if(it>>6 != 0b10) return false;
+                cnt--;
+            }
+            
+        }
+        return cnt==0;
+        
+    }
+};
