29878e336b79087a265eb2af28e69ded824dae17
Chandan Giri <jpgiri017@gmail.com>
2022-09-20 17:16:07 +0530

    Time: 890 ms (10.63%), Space: 109.3 MB (14.62%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int n,m , maxLen = 0;
     vector<vector<int>> dp;
-    int solve(vector<int> &nums1 , vector<int> &nums2 , int ind1 , int ind2)
+    int solve(vector<int> &nums1 , vector<int> &nums2)
     {
-        if(ind1>=n || ind2>=m)
-           return 0;
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(nums1[i-1] == nums2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = 0;
+                
+                maxLen = max(maxLen , dp[i][j]);
+            }
+        }
         
-        if(dp[ind1][ind2] != -1)
-            return dp[ind1][ind2];
+        return maxLen;
         
         
-        if(nums1[ind1] == nums2[ind2])
-            dp[ind1][ind2] = 1 + solve(nums1 , nums2 , ind1+1 ,ind2+1);
-        else
-            dp[ind1][ind2] = 0;
-        solve(nums1 , nums2 ,ind1+1 , ind2);
-        solve(nums1 , nums2 , ind1 , ind2+1);
-        
-        maxLen = max(maxLen , dp[ind1][ind2]);
-        return dp[ind1][ind2];
+       
     }
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         
         n = nums1.size() , m = nums2.size();
-        dp.resize(n , vector<int> (m , -1));
-        int res = solve(nums1 , nums2 , 0 , 0);
+        int res = solve(nums1 , nums2);
        
         return maxLen;
         
