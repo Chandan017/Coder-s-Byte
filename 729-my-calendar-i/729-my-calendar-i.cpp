b163534ccf034170d631fc9811f4dbe44eb424c9
Chandan Giri <jpgiri017@gmail.com>
2022-10-07 22:27:02 +0530

    Time: 2280 ms (5.01%), Space: 430.3 MB (5.90%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,26 @@
+class MyCalendar {
+public:
+    vector<vector<int>> nums;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+                
+        for(auto it:nums)
+        {
+            if(start < it[1] && it[0] <end)
+                return false;
+        }
+        
+        nums.push_back({start , end});
+        return true;
+        
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
