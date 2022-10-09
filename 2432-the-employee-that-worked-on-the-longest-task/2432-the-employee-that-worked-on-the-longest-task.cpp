d04e8041f00eded493a17e88d471c11d9182008a
Chandan Giri <jpgiri017@gmail.com>
2022-10-09 21:58:26 +0530

    Time: 99 ms (25.00%), Space: 38.7 MB (25.00%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -2,27 +2,25 @@ class Solution {
 public:
     int hardestWorker(int n, vector<vector<int>>& logs) {
         
-        int prev = 0;
-        vector<int> id(n,0);
+        int prev = 0 , longestTask = -1 , empID = INT_MAX;
         
         for(auto it:logs)
         {
             int currTime = it[1] - prev;
             
-            id[it[0]] = max(currTime , id[it[0]]);
+            if(currTime > longestTask)
+            {
+                longestTask = currTime;
+                empID = it[0];
+            }
+            else if(currTime == longestTask)
+            {
+                empID = min(empID , it[0]);
+            }
             prev = it[1];
         }
         
-        int longestTask = -1 , empID = INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            if(id[i] > longestTask)
-            {
-                longestTask = id[i];
-                empID = i;
-            }   
-        }
-        
+      
         return empID;
         
     }
