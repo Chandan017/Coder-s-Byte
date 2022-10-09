1416dc60d4aca944f4868dc4fc870e04c9573a93
Chandan Giri <jpgiri017@gmail.com>
2022-10-09 21:55:53 +0530

    Time: 196 ms (25.00%), Space: 39.5 MB (25.00%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        
+        int prev = 0;
+        vector<int> id(n+1,0);
+        
+        for(auto it:logs)
+        {
+            int currTime = it[1] - prev;
+            
+            id[it[0]] = max(currTime , id[it[0]]);
+            prev = it[1];
+        }
+        
+        int longestTask = -1 , empID = INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            if(id[i] > longestTask)
+            {
+                longestTask = id[i];
+                empID = i;
+            }   
+        }
+        
+        return empID;
+        
+    }
+};
