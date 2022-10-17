8de50b67fd5cc753d2fe4c8c69d1c3c62e9f9799
Chandan Giri <jpgiri017@gmail.com>
2022-10-17 15:22:35 +0530

    Time: 294 ms (7.95%), Space: 16.7 MB (38.14%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,39 +2,23 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        map<char,int> mpp1 , mpp2;
+        unordered_map<char,int> mpp;
         
-        for(auto c:s)
+        for(auto &c:s)
         {
-            mpp1[c]++;
+            mpp[c]++;
         }
         
-//         for(auto it:mpp1)
-//         {
-//             cout<<it.first<<"->"<<it.second<<"  ";
-//             // totalConversions += it.second;
-//         }
-        
-        for(auto c:t)
+        for(auto &c:t)
         {
-            if(mpp1.find(c) != mpp1.end() && mpp1[c] > 0)
-            {
-                mpp1[c]--;
-            }
-            
+            if(mpp.find(c) != mpp.end() && mpp[c] > 0)
+                mpp[c]--;
         }
-        
         
         int totalConversions = 0;
         
-        // cout<<endl;
-        for(auto it:mpp1)
-        {
-            // cout<<it.first<<"->"<<it.second<<"  ";
+        for(auto it:mpp)
             totalConversions += it.second;
-        }
-        
-      
         
         return totalConversions;
         
