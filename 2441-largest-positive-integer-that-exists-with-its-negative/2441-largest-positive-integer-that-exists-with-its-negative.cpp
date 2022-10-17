5e588a88938f90306eb04c310d7dfbdd06d57dce
Chandan Giri <jpgiri017@gmail.com>
2022-10-17 22:38:46 +0530

    Time: 53 ms (55.56%), Space: 22.9 MB (11.11%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -9,17 +9,18 @@ public:
         for(auto it:nums)
         {
             if(it < 0)
-                st.insert(it);
-        }
-        
-        for(auto it:nums)
-        {
-            if(it > 0 && st.find(-it) != st.end() && it > maxi)
+            {               
+                if(st.find(abs(it)) != st.end())
+                    maxi = max(maxi , abs(it));
+            }
+            else if(st.find(-it) != st.end() && it > maxi)
                 maxi = it;
-          
+           
+            
+            st.insert(it);
         }
         
-      
+       
         return maxi;
     }
 };
