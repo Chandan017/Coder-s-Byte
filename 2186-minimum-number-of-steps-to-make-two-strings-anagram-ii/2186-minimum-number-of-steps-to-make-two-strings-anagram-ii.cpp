93ba71d5251fac4e1328d2ba0f9b707605002f68
Chandan Giri <jpgiri017@gmail.com>
2022-10-17 22:27:27 +0530

    Time: 328 ms (43.40%), Space: 28.8 MB (89.61%) - LeetHub

--- 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
+++ 2186-minimum-number-of-steps-to-make-two-strings-anagram-ii/2186-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -2,26 +2,26 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         
-        vector<int> freq1(26,0) , freq2(26,0);
+        vector<int> freq(26,0) ;
         
         for(auto c:s)
         {
-            freq1[c-'a']++;
-        }
-        
-        for(auto c:t)
-        {
-            if(freq1[c-'a'] > 0)
-                freq1[c-'a']--;
-            else
-                freq2[c-'a']++;
+            freq[c-'a']++;
         }
         
         int minCnt = 0;
         
+        for(auto c:t)
+        {
+            if(freq[c-'a'] > 0)
+                freq[c-'a']--;
+            else
+                minCnt++;
+        }
+        
         for(int i=0;i<26;i++)
         {
-            minCnt += freq1[i] + freq2[i];
+            minCnt += freq[i];
         }
         
         return minCnt;
