4b3902c30c794bad45b45366e444ceec952742f0
Chandan Giri <jpgiri017@gmail.com>
2022-10-18 17:37:34 +0530

    Time: 7 ms (82.28%), Space: 6.6 MB (77.40%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        if(n == 1)
+            return "1";
+        
+        string curr = countAndSay(n-1);
+        
+        string res = "";
+        char prev = curr[0];
+        int freq = 0 , len = curr.length();
+        for(int i=0;i<=len;i++)
+        {
+            if(curr[i] != prev || i == len)
+            {
+                res += (to_string(freq));
+                
+                res += prev;
+                
+                if(i != len)
+                    prev = curr[i];
+                freq = 1;
+            }
+            else
+                freq++;
+        }
+        
+        
+        return res;
+        
+    }
+};
