e333eeb17c4cd99dcedaa6d3061e0f7bf07c1356
Chandan Giri <jpgiri017@gmail.com>
2022-10-21 01:02:42 +0530

    Time: 496 ms (12.90%), Space: 40.4 MB (65.81%) - LeetHub

---
+++ 1702-maximum-binary-string-after-change/1702-maximum-binary-string-after-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string maximumBinaryString(string bin) {
+        
+        int n = bin.length();
+        int i= 0 , ind = n-1;
+        
+        while(i<n && bin[i]=='1')
+            i++;
+        
+        ind = i;
+        int cnt = 0;
+        
+        while(i<n)
+        {
+            if(bin[i] == '0')
+                cnt++;
+            i++;
+        }
+        while(ind<n)
+        {
+            if(cnt > 1)
+                bin[ind] = '1';
+            else if(cnt == 1)
+                bin[ind] = '0';
+            else
+                bin[ind] = '1';
+            cnt--;
+            ind++;
+            
+        }
+        cout<<ind<<" "<<cnt<<endl;
+        return bin;
+        
+    }
+};
