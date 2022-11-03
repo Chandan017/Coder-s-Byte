59d0dab8a8ef721613340c07c7ef486d7569a860
Chandan Giri <jpgiri017@gmail.com>
2022-11-04 00:16:58 +0530

    Time: 1082 ms (78.56%), Space: 134.6 MB (98.84%) - LeetHub

---
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<string>> mostPopularCreator(vector<string>& creators, vector<string>& ids, vector<int>& views) {
+        
+        vector<vector<string>> res;
+        
+        unordered_map<string,long> mpp;
+        
+        int n = ids.size();
+        
+        long maxView = 0;
+        for(int i=0;i<n;i++)
+        {
+            mpp[creators[i]] += views[i];
+            maxView = max(maxView , mpp[creators[i]]);
+        }
+        
+        map<string,set<pair<int,string>>> temp;
+        
+        for(int i=0;i<n;i++)
+        {
+            temp[creators[i]].insert({ -views[i] ,ids[i]});
+        }
+        
+        // set<> st;
+        
+        for(int i=0;i<n;i++)
+        {
+            if(mpp[creators[i]] == maxView)
+            {
+                
+                auto curr = temp[creators[i]].begin();
+                
+                res.push_back({creators[i] , curr->second});
+                mpp[creators[i]] = -1;
+            }
+        }
+        
+        // vector<tuple<long,string,string>> temp;
+        
+        
+        
+        return res;
+        
+    }
+};
