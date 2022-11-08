1659b744a5d271818f5d7d7be044937f49764e31
Chandan Giri <jpgiri017@gmail.com>
2022-11-08 18:06:09 +0530

    Time: 578 ms (52.91%), Space: 94.5 MB (47.61%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -8,37 +8,25 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(st.find(nums[i]) == st.end())
+            while(prev<n && st.find(nums[i]) != st.end())
             {
-                st.insert(nums[i]);
-                sum += nums[i];
+                sum -= nums[prev];
+                st.erase(st.find(nums[prev]));
+                prev++;     
             }
-            else
-            {
-                while(prev<n && st.find(nums[i]) != st.end())
-                {
-                    sum -= nums[prev];
-                    st.erase(st.find(nums[prev]));
-                    prev++;
-                    
-                }
                 
-                st.insert(nums[i]);
-                sum+= nums[i];
-            }
+            st.insert(nums[i]);
+            sum+= nums[i];
             
             if(i-prev == k-1)
             {
                 maxSum = max(sum , maxSum);
-                
                 sum -= nums[prev];
                 st.erase(st.find(nums[prev]));
                 prev++;
-                // cout<<maxSum<<" ";
             }
         }
         
-        // cout<<endl;
         
         return maxSum;
         
