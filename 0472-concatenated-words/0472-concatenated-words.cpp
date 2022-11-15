364e89ddfcef757af8f9cbcafae307e0bdf387a6
Chandan Giri <jpgiri017@gmail.com>
2022-11-15 17:27:49 +0530

    Time: 794 ms (19.23%), Space: 233.8 MB (13.75%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,123 @@
+class Solution {
+public:
+    
+    
+    
+    class Node{
+    public:
+        Node* links[26];
+        bool flag = false;
+        
+        bool containsKey(char ch)
+        {
+            return links[ch-'a'];
+        }
+        
+        void putKey(char ch)
+        {
+            links[ch-'a'] = new Node();
+        }
+        
+        Node* nextLink(char ch)
+        {
+            return links[ch-'a'];
+        }
+        
+        void setEnd()
+        {
+            flag = true;
+        }
+        
+        bool isEnd()
+        {
+            return flag;
+        }
+        
+    };
+    
+    class Trie{
+    public:
+        Node* root = new Node();
+        
+        void insert(string &word)
+        {
+            Node* dummy = root;
+            
+            for(auto it:word)
+            {
+                if(!dummy->containsKey(it))
+                    dummy->putKey(it);
+                dummy = dummy->nextLink(it);
+            }
+            
+            dummy->setEnd();
+        }
+        
+        bool search(string &word)
+        {
+            Node* dummy = root;
+            
+            for(auto it:word)
+            {
+                if(dummy->containsKey(it))
+                    dummy = dummy->nextLink(it);
+                else
+                    return false;
+            }
+            
+            return dummy->isEnd();
+        }
+        
+        bool find(string word , string curr , int ind , int cnt)
+        {
+            if(ind == word.length())
+            {
+                
+                if(cnt >= 2 && curr == "")
+                    return true;
+                return false;
+            }
+            
+            curr += word[ind];
+            ind += 1;
+            if(search(curr))
+            {
+                // cout<<curr<<" ";
+                bool takeIt = find(word , "" , ind, cnt+1);
+                
+                if(takeIt)
+                    return true;
+            }
+            
+            bool notTakeIt = find(word , curr , ind, cnt);
+            
+            return notTakeIt;
+        }
+        
+    };
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        
+        
+        vector<string> res;
+        
+        Trie* trie = new Trie();
+        
+        for(auto it:words)
+        {
+            trie->insert(it);
+        }
+//         if(trie->find("abc" , "" , 0 , 0))
+//                 res.push_back("abc");
+        
+        for(auto it:words)
+        {
+            if(trie->find(it , "" , 0 , 0))
+                res.push_back(it);
+        }
+        
+
+        
+        return res;
+        
+    }
+};
