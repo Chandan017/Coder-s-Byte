e6a391ceff8b71fe6f20fb9605b7d2fe6fd52407
Chandan Giri <jpgiri017@gmail.com>
2022-11-16 21:11:53 +0530

    Time: 6 ms (74.16%), Space: 12.2 MB (11.21%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
     
-    int solve(vector<int> &prices , int ind , bool buy , vector<vector<int>> &dp)
+    int solve(vector<int> &prices , int ind , bool canBuy , vector<vector<int>> &dp)
     {
         if(ind >= prices.size())
             return 0;
         
-        if(dp[ind][buy] != -1)
-            return dp[ind][buy];
+        if(dp[ind][canBuy] != -1)
+            return dp[ind][canBuy];
         
-        int notTakeIt = solve(prices , ind+1 , buy , dp);
-        int takeIt = 0;
+        int cooldown = solve(prices , ind+1 , canBuy , dp);
+        int buyOrSell = 0;
         
-        if(buy)
+        if(canBuy)
         {
-            takeIt = -prices[ind] + solve(prices , ind+1 , false , dp);
+            buyOrSell = -prices[ind] + solve(prices , ind+1 , false , dp);
         }
         else
         {
-            takeIt = prices[ind] + solve(prices , ind+2 , true , dp);
+            buyOrSell = prices[ind] + solve(prices , ind+2 , true , dp);
         }
         
         
-        return dp[ind][buy] =  max(takeIt , notTakeIt);
-        
+        return dp[ind][canBuy] = max(cooldown , buyOrSell);
+            
     }
     int maxProfit(vector<int>& prices) {
         
         
-        vector<vector<int>> dp(prices.size() , vector<int>(2 , -1));
-        
+        int n = prices.size();
+        vector<vector<int>> dp(n , vector<int> (2 , -1));
         
         int res = solve(prices , 0 , true , dp);
         
