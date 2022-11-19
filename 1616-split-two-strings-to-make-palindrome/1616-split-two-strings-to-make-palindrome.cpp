58fc17d5f3df709693cf81bb319f1e0b1e602f93
Chandan Giri <jpgiri017@gmail.com>
2022-11-19 17:39:55 +0530

    Time: 65 ms (98.74%), Space: 29.4 MB (55.65%) - LeetHub

---
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+
+
+
+    bool isPalindrome(string &s)
+    {
+        int l = 0 , h = s.length()-1;
+
+        while(l<h)
+        {
+            if(s[l] != s[h])
+                return false;
+            l++;
+            h--;
+        }
+
+        return true;
+    }
+    bool checkPalindromeFormation(string a, string b) {
+
+        int n = a.length();
+
+        if(isPalindrome(a) || isPalindrome(b))
+            return true;
+        int i=0,j=n-1;
+
+        while(i<n && j>=0)
+        {
+            if(a[i] == b[j])
+            {
+                i++;
+                j--;
+            }
+            else
+                break;
+        }
+
+        if(i>=j)
+            return true;
+        // cout<<n<<" "<<i<<" "<<j<<endl;
+        
+        string temp = b.substr(i , j-i+1);
+        string temp2 = a.substr(i , j-i+1);
+        // cout<<temp<<" "<<temp2<<endl;
+        if(isPalindrome(temp) || isPalindrome(temp2))
+            return true;
+        
+        i=0,j=n-1;
+
+        while(i<n && j>=0)
+        {
+            if(b[i] == a[j])
+            {
+                i++;
+                j--;
+            }
+            else
+            break;
+        }
+
+
+        if(i>=j)
+        return true;
+        
+        temp = b.substr(i , j-i+1);
+        temp2 = a.substr(i , j-i+1);
+        
+        if(isPalindrome(temp) || isPalindrome(temp2))
+            return true;
+
+        return false;
+        
+    }
+};
