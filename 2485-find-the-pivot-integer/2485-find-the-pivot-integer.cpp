d35228e9fdedc791a383c65f8ff23ad14902f51e
Chandan Giri <jpgiri017@gmail.com>
2022-11-27 23:00:27 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (45.45%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -5,18 +5,15 @@ public:
         int sum = (n*(n+1)) / 2;
         
         int x = 1;
-        // cout<<sum<<"-> ";
+        
         while(x<=n)
         {
             int curr =(x*(x+1))/2;
-            // cout<<curr<<"->";
-            // cout<<(sum - curr + x)<<"  ";
+
             if(curr == (sum - curr + x))
                 return x;
             x++;
         }
-        
-        // cout<<endl;
         
         return -1;
     }
