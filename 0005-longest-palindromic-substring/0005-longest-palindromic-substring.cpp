da0f7eb1c7f52220fe4739d277352add97b13552
Chandan Giri <jpgiri017@gmail.com>
2022-11-28 21:47:02 +0530

    Time: 1997 ms (5.00%), Space: 122.4 MB (25.91%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool isPalindrome(string &s)
+    {
+        int l = 0 , h = s.length()-1;
+        
+        while(l<h)
+        {
+            if(s[l] != s[h])
+                return false;
+            l++;
+            h--;
+        }
+        
+        return true;
+    }
+    
+    string longestPalindrome(string s) {
+        
+        int n = s.length();
+        
+        string res = "";
+        for(int i=0;i<n;i++)
+        {
+            string curr = "";
+            
+            for(int j=i;j<n;j++)
+            {
+                curr += s[j];
+                
+                if(isPalindrome(curr) && curr.length() > res.length())
+                    res = curr;
+            }
+        }
+        
+        return res;
+        
+    }
+};
