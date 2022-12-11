3956488b4428f54bd3e9f2e4ed6d64605ff3251b
Chandan Giri <jpgiri017@gmail.com>
2022-12-11 23:47:13 +0530

    Time: 32 ms (66.26%), Space: 11.7 MB (86.68%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin() , intervals.end());
+        
+        priority_queue<int,vector<int> , greater<int>> minEndTime;
+        
+        minEndTime.push(intervals[0][1]);
+        int rooms = 1 ;
+        // for(auto it:intervals)
+        // {
+        //     for(auto j:it)
+        //         cout<<j<<" ";
+        //     cout<<endl;
+        // }
+        
+        for(int i=1;i<intervals.size();i++)
+        {
+            int minTime = minEndTime.top();
+            cout<<minTime<<" ";
+                
+            if(intervals[i][0] < minTime)
+                rooms++;
+            else
+                minEndTime.pop();
+            minEndTime.push(intervals[i][1]);
+        }
+        
+        return rooms;
+    }
+};
