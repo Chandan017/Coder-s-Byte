1955e0459abdd374244ad0953d68679527e629f4
Chandan Giri <jpgiri017@gmail.com>
2022-12-12 01:01:32 +0530

    Time: 313 ms (58.28%), Space: 55 MB (5.16%) - LeetHub

---
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        
+        map<char,int> mpp;
+        int prev = 0 , maxLen = 0;
+        
+        for(int i=0;i<s.length();i++)
+        {
+            mpp[s[i]]++;
+            
+            if(mpp.size() <= 2)
+            {
+                int currLen = i-prev+1;
+                
+                maxLen = max(maxLen , currLen);
+                cout<<currLen<<" ";
+            }
+            else
+            {
+                while(prev<i && mpp.size()>2)
+                {
+                    mpp[s[prev]]--;
+                    
+                    if(mpp[s[prev]] == 0)
+                        mpp.erase(s[prev]);
+                    prev++;
+                }
+            }
+        }
+        cout<<endl;
+        return maxLen;
+        
+    }
+};
