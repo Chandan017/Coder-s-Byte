bd914f6ad12f1c4d2e89ec331f78a09f8e017791
Chandan Giri <jpgiri017@gmail.com>
2022-12-14 00:08:30 +0530

    Time: 632 ms (35.41%), Space: 67.4 MB (89.84%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.cpp
@@ -0,0 +1,50 @@
+class FileSystem {
+public:
+    unordered_map<string,int> mpp;
+    FileSystem() {
+        
+    }
+    
+    bool createPath(string path, int value) {
+        
+        if(mpp.find(path) != mpp.end())
+            return false;
+        
+        string curr = path;
+        while(path.back() != '/')
+        {
+            path.pop_back();
+        }
+        
+        path.pop_back();
+        
+        if(path.size() > 1)
+        {
+            if(mpp.find(path) == mpp.end())
+                return false;
+        }
+        
+        
+        
+        // curr += '/';
+        // reverse(curr.begin() , curr.end());
+        mpp[curr] = value;
+        return true;
+        
+    }
+    
+    int get(string path) {
+        
+        if(mpp.find(path) == mpp.end())
+            return -1;
+        
+        return mpp[path];
+    }
+};
+
+/**
+ * Your FileSystem object will be instantiated and called as such:
+ * FileSystem* obj = new FileSystem();
+ * bool param_1 = obj->createPath(path,value);
+ * int param_2 = obj->get(path);
+ */
