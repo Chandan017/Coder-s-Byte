14df061cec9d00475f0b68bb3547d56eb559416e
Chandan Giri <jpgiri017@gmail.com>
2022-12-17 18:50:51 +0530

    Time: 59 ms (92.16%), Space: 31.6 MB (39.22%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
@@ -8,9 +8,7 @@ public:
         for(int i=0;i<n-1;i++)
         {
             int diff = nums[i+1]-nums[i] - 1;
-            
-            // cnt += diff;
-            
+                        
             if(diff >= k)
                 return nums[i]+k;
             
