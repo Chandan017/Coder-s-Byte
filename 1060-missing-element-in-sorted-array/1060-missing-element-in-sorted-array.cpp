f35f9a2a7c8737a336e6e2e53d743c498196daec
Chandan Giri <jpgiri017@gmail.com>
2022-12-17 18:49:11 +0530

    Time: 64 ms (87.68%), Space: 31.6 MB (5.32%) - LeetHub

---
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int missingElement(vector<int>& nums, int k) {
+        
+        
+        int cnt = 0;
+        int n = nums.size();
+        for(int i=0;i<n-1;i++)
+        {
+            int diff = nums[i+1]-nums[i] - 1;
+            
+            // cnt += diff;
+            
+            if(diff >= k)
+                return nums[i]+k;
+            
+            k -= diff;
+        }
+        
+        return nums[n-1]+k;
+        
+    }
+};
