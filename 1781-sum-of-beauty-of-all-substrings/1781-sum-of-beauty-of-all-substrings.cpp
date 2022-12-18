7a09e337f89a4feb040401dc3538da2d06212264
Chandan Giri <jpgiri017@gmail.com>
2022-12-19 00:45:03 +0530

    Time: 456 ms (71.11%), Space: 23.7 MB (26.53%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    
+    int getBeauty(unordered_map<char,int> &mpp)
+    {
+        int maxi = INT_MIN , mini = INT_MAX;
+        
+        for(auto it:mpp)
+        {
+            maxi = max(maxi , it.second);
+            mini = min(mini , it.second);
+        }
+        
+        return maxi-mini;
+    }
+    int beautySum(string s) {
+        
+        int res = 0 , n = s.length();
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            unordered_map<char,int> freq;
+            for(int j=i;j<n;j++)
+            {
+                freq[s[j]]++;
+                
+                if(freq.size() > 1)
+                {
+                    int currBeauty = getBeauty(freq);
+                    
+                    res += currBeauty;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
