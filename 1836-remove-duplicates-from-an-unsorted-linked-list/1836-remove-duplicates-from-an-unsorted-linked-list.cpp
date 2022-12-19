80229ca2cbf8f1da506679b63d531bb4c82077bd
Chandan Giri <jpgiri017@gmail.com>
2022-12-19 19:06:28 +0530

    Time: 1033 ms (68.38%), Space: 177.1 MB (16.20%) - LeetHub

---
+++ 1836-remove-duplicates-from-an-unsorted-linked-list/1836-remove-duplicates-from-an-unsorted-linked-list.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicatesUnsorted(ListNode* head) {
+        
+        unordered_map<int,int> mpp;
+        ListNode* temp = head;
+        
+        while(temp)
+        {
+            mpp[temp->val]++;
+            temp = temp->next;
+        }
+        
+        ListNode* newHead = new ListNode(0);
+        ListNode* dummy = newHead;
+        temp = head;
+        
+        while(temp)
+        {
+            if(mpp[temp->val] == 1)
+            {
+                dummy->next = new ListNode(temp->val);
+                dummy = dummy->next;
+            }
+            
+            temp = temp->next;
+                
+        }
+        
+        
+        
+        
+        newHead = newHead->next;
+        return newHead;
+        
+    }
+};
