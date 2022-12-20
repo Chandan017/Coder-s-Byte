ef7fe78f36124961d342e0752fcc1e630d1b5d21
Chandan Giri <jpgiri017@gmail.com>
2022-12-20 19:17:24 +0530

    Time: 10 ms (80.41%), Space: 8.9 MB (59.62%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> parent;
+    int getParent(int x)
+    {
+        if(parent[x] == x)
+            return x;
+        return getParent(parent[x]);
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+        int n = edges.size();
+        parent.resize(n+1 , 0);
+        
+        for(int i=0;i<=n;i++)
+            parent[i] = i;
+        
+        vector<int> res;
+        for(auto it:edges)
+        {
+            int firstParent = getParent(it[0]);
+            int secondParent = getParent(it[1]);
+            
+            // cout<<firstParent<<"->"<<secondParent<<endl;
+            if(firstParent != secondParent)
+            {
+                parent[firstParent] = secondParent;
+            }
+            else
+            {
+                res.push_back(it[0]);
+                res.push_back(it[1]);
+                break;
+            }
+        }
+        
+        return res;
+        
+    }
+};
