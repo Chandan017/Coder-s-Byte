f7c02053f23c4c9796c5dfca1462fbbc0f294c2e
Chandan Giri <jpgiri017@gmail.com>
2022-12-20 23:19:53 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (30.94%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int solve(vector<int> &nums , int target , vector<int> &dp)
+    {
+        if(target == 0)
+            return 1;
+        if(dp[target] != -1)
+            return dp[target];
+        
+        int cnt = 0;
+        for(auto it:nums)
+        {
+            if(target - it>=0)
+            {
+                cnt += solve(nums , target-it , dp);
+            }
+        }
+        
+        dp[target] = cnt;
+        
+        return cnt;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        vector<int> dp(target+1 , -1);
+        int res = solve(nums , target , dp);
+        
+        return res;
+        
+    }
+};
