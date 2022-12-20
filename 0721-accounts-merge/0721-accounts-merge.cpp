cb5b33c8a1924b2ac4056ccbd8b6e1cc44e47872
Chandan Giri <jpgiri017@gmail.com>
2022-12-20 19:52:16 +0530

    Time: 205 ms (54.32%), Space: 51.8 MB (10.94%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    map<string,vector<string>> adj;
+    set<string> vis;
+
+
+    void takeAll(string node , vector<string> &curr)
+    {
+        vis.insert(node);
+        curr.push_back(node);
+        
+        for(auto it:adj[node])
+        {
+            if(vis.find(it) == vis.end())
+            {
+                takeAll(it , curr);
+            }
+        }
+        
+        return ;
+        
+    }
+    vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+        
+        
+        for(auto it:accounts)
+        {
+            string firstMail = it[1];
+            
+            for(int i=2;i<it.size();i++)
+            {
+                adj[firstMail].push_back(it[i]);
+                adj[it[i]].push_back(firstMail);
+            }
+        }
+        
+        
+        vector<vector<string>> res;
+        
+        for(auto it:accounts)
+        {
+            string name = it[0] , firstMail = it[1];
+            
+            if(vis.find(firstMail) == vis.end())
+            {
+                vector<string> curr;
+                takeAll(firstMail , curr);
+                
+                sort(curr.begin() , curr.end());
+                curr.insert(curr.begin() , name);
+                
+                res.push_back(curr);
+            }
+            
+        }
+        
+        
+        return res;
+        
+        
+        
+    }
+};
