45a45b854d0b3c2629156a97d33bfdc294088ff6
Chandan Giri <jpgiri017@gmail.com>
2022-12-22 19:42:12 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (71.24%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.cpp
+++ 0276-paint-fence/0276-paint-fence.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-    int solve(int n , int k)
+    int solve(int n , int k , vector<int> &dp)
     {
         if(n==1)
             return k;
         if(n == 2)
             return k*k;
         
-       int ans = (k-1)*solve(n-1 , k) + (k-1)*solve(n-2 , k);
+        if(dp[n] != -1)
+            return dp[n];
+        
+       int ans = (k-1)*solve(n-1 , k , dp) + (k-1)*solve(n-2 , k , dp);
     
+        dp[n] = ans;
         return ans;
         
     }
     int numWays(int n, int k) {
         
-        int res = solve(n , k );
+        vector<int> dp(n+1 , -1);
+        int res = solve(n , k , dp);
         
         return res;
     }
