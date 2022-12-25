34ad9deec951825879dfd2d16e4b1f410e9d9b7b
Chandan Giri <jpgiri017@gmail.com>
2022-12-25 23:02:14 +0530

    Time: 106 ms (49.28%), Space: 24.1 MB (47.83%) - LeetHub

--- 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
+++ 1167-minimum-cost-to-connect-sticks/1167-minimum-cost-to-connect-sticks.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
-    vector<int> parent;
-    
-    int getParent(int x)
-    {
-        if(parent[x] == x)
-            return x;
-        
-        return getParent(parent[x]);
-    }
+
     int connectSticks(vector<int>& sticks) {
         
-        priority_queue<vector<int> , vector<vector<int>> , greater<vector<int>>> pq;
+        priority_queue<int , vector<int> , greater<int>> pq;
         int n = sticks.size();
         
         for(int i=0;i<n;i++)
         {
-            pq.push({sticks[i] , i});
+            pq.push(sticks[i]);
         }
-        
-        parent.resize(n);        
-        for(int i=0;i<n;i++)
-            parent[i] = i;
         
         int minCost = 0;
         
-       while(pq.size())
+       while(pq.size() > 1)
        {
-           auto x = pq.top();
+           int cost = pq.top();
+           pq.pop();
+           cost += pq.top();
            pq.pop();
            
-           if(pq.size())
-           {
-               auto y = pq.top();
-               pq.pop();
-               
-               if(getParent(x[1]) != getParent(y[1]))
-               {
-                   minCost += (x[0]+y[0]);
-                   parent[x[1]] = y[1];
-                   
-                   pq.push({(x[0] + y[0]) , x[1]});
-               }
-           }
+           minCost += cost;
+           pq.push(cost);
        }
         
         return minCost;
