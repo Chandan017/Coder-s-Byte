41aa2252300d0060a2eb110d92e5e00a59cab246
Chandan Giri <jpgiri017@gmail.com>
2022-12-30 00:00:46 +0530

    Time: 19 ms (62.11%), Space: 11.5 MB (47.89%) - LeetHub

---
+++ 2510-check-if-there-is-a-path-with-equal-number-of-0s-and-1s/2510-check-if-there-is-a-path-with-equal-number-of-0s-and-1s.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int n,m;
+    bool dp[101][101][200] = {};
+    bool check(vector<vector<int>> &grid , int r , int c , int target)
+    {
+        if(r>=n || c>=m || target<0 || dp[r][c][target])
+            return false;
+        
+        if(dp[r][c][target])
+            return dp[r][c][target];
+        
+        dp[r][c][target] = true;
+        
+        target -= grid[r][c];
+        
+
+        if(r==n-1 && c==m-1)
+        {
+            if(target == 0)
+                return true;
+            
+            return false;
+        }
+        
+        return check(grid , r , c+1 , target) ||  check(grid , r+1 , c , target);
+        
+    }
+    
+    bool isThereAPath(vector<vector<int>>& grid) {
+     
+        n = grid.size() , m = grid[0].size();
+        
+        if((n+m-1) % 2 != 0)
+            return false;
+        
+        bool res = check(grid , 0 , 0 , (n+m-1)/2);
+
+        return res;
+    }
+};
