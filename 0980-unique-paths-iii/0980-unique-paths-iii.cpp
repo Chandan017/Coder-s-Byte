6c5db9e1804e450f38b9768940bb8bd79d3209a4
Chandan Giri <jpgiri017@gmail.com>
2022-12-31 20:23:34 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (64.25%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int totalBoxes = 0 , n , m , ways = 0;
+    
+    int dx[4] = {-1,1,0,0};
+    int dy[4] = {0,0,-1,1};
+    
+    void solve(vector<vector<int>> &grid , int r , int c , int coveredBoxes)
+    {
+        if(r<0 || c<0 || r>=n || c>=m || grid[r][c] == -1)
+            return ;
+        
+        if(grid[r][c] == 2)
+        {
+            // cout<<coveredBoxes<<" ";
+            if(coveredBoxes == totalBoxes)
+                ways++;
+            return ;
+        }
+        
+        grid[r][c] = -1;
+        
+        for(int ind=0;ind<4;ind++)
+        {
+            int newR = r+dx[ind];
+            int newC = c+dy[ind];
+            
+            solve(grid , newR , newC , coveredBoxes+1);
+        }
+        
+        grid[r][c] = 0;
+        
+        return ;
+        
+        
+    }
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        
+        n = grid.size() , m = grid[0].size();
+        
+        int sx , sj;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j] != -1)
+                    totalBoxes++;
+                
+                if(grid[i][j] == 1)
+                {
+                    sx = i;
+                    sj = j;
+                }
+            }
+        }
+        
+        // cout<<totalBoxes<<endl;?
+        
+        solve(grid , sx , sj , 1);
+        
+        return ways;
+        
+    }
+};
