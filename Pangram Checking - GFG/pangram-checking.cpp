4310bb4dfbe351b7cefd2b39631f36610fbe0439
Chandan Giri <jpgiri017@gmail.com>
2023-01-26 23:04:02 +0530

    Added solution - LeetHub

---
+++ Pangram Checking - GFG/pangram-checking.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution
+{
+  public:
+    //Function to check if a string is Pangram or not.
+    bool checkPangram (string &str) {
+        // your code here
+        
+        unordered_set<char> st;
+        
+        for(auto it:str)
+        {
+            if(isupper(it) || islower(it))
+                st.insert(tolower(it));
+        }
+        
+        return (st.size()==26);
+    }
+
+};
+
+//{ Driver Code Starts.
+
+// Driver Program to test above functions
+int main()
+{
+    int t;
+    cin>>t;
+    cin.ignore(INT_MAX, '\n');
+    while(t--){
+        string str;
+        getline(cin, str);
+        Solution obj;
+        if (obj.checkPangram(str) == true)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+    return(0);
+}
+
+// } Driver Code Ends
