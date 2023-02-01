db3dbbe51c1bf3a742e86513a4765e313bb204c8
Chandan Giri <jpgiri017@gmail.com>
2023-02-01 21:52:08 +0530

    Added solution - LeetHub

--- Distinct Coloring - GFG/distinct-coloring.cpp
+++ Distinct Coloring - GFG/distinct-coloring.cpp
@@ -9,43 +9,52 @@ using namespace std;
 
 class Solution{   
 public:
-    long long int f(int ind,int N,int r[],int g[],int b[],int prev,vector<vector<long long int>>& dp)
+
+    long long int solve(int ind , int N , int r[] , int g[] , int b[] , int prev , vector<vector<long long int>> &dp)
     {
-        if(ind==N) return 0;
+        if(ind == N)
+            return 0;
+            
+        if(dp[ind][prev] != -1)
+            return dp[ind][prev];
         
-        if(dp[ind][prev]!=-1) return dp[ind][prev];
+        long long int red = 1e11; 
+        long long int blue = 1e11;
+        long long int green = 1e11;
         
-        long long int taker=1e11,takeg=1e11,takeb=1e11;
-        if(prev==0) // If no color painted
+        if(prev == 0)
         {
-            taker=r[ind]+f(ind+1,N,r,g,b,1,dp);
-            takeg=g[ind]+f(ind+1,N,r,g,b,2,dp);
-            takeb=b[ind]+f(ind+1,N,r,g,b,3,dp);
+            red = r[ind] + solve(ind+1 ,N, r , g, b, 1 ,dp);
+            green = g[ind] + solve(ind+1 ,N, r, g, b , 2,dp);
+            blue = b[ind] + solve(ind+1 , N,r , g, b , 3,dp);
         }
-        else if(prev==1) // If red color painted
+        else if(prev == 1)
         {
-            takeg=g[ind]+f(ind+1,N,r,g,b,2,dp);
-            takeb=b[ind]+f(ind+1,N,r,g,b,3,dp);
+            green = g[ind] + solve(ind+1 , N , r , g , b , 2,dp);
+            blue = b[ind] + solve(ind+1 , N , r , g , b , 3,dp);
         }
-        else if(prev==2) // If Green Color painted
+        else if(prev == 2)
         {
-            taker=r[ind]+f(ind+1,N,r,g,b,1,dp);
-            takeb=b[ind]+f(ind+1,N,r,g,b,3,dp);
+            red = r[ind] + solve(ind+1 , N , r , g , b , 1,dp);
+            blue = b[ind] + solve(ind+1 , N , r , g , b , 3,dp);
         }
-        else // If Blue Color painted
+        else
         {
-            takeg=r[ind]+f(ind+1,N,r,g,b,1,dp);
-            takeb=g[ind]+f(ind+1,N,r,g,b,2,dp);
+            red = r[ind] + solve(ind+1 , N , r , g , b , 1,dp);
+            green = g[ind] + solve(ind+1 , N , r , g , b , 2,dp);
         }
-        return dp[ind][prev]=min(taker,min(takeb,takeg));
+        
+        return dp[ind][prev] = min(red , min(green , blue));
+       
     }
     long long int distinctColoring(int N, int r[], int g[], int b[]){
-        vector<vector<long long int>>  dp(N,vector<long long int>(4,-1));
-        return f(0,N,r,g,b,0,dp);
-        // 0 -> no color
-        // 1 -> red color
-        // 2 -> green color
-        // 3 -> blue color
+        // code here 
+        vector<vector<long long int>> dp(N+1 , vector<long long int> (4,-1));
+        long long int res = solve(0 , N , r , g , b, 0,dp);
+        
+        return res;
+        
+        
     }
 };
 
